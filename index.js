09b61cf2f4946369ea0a95919756dccf601e837a
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-13 17:33:51 -0600

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => {renderBooks(json); totalPageCount(json); fifthInTheSeries(json); console.log(json)});
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main')
@@ -11,6 +15,37 @@ function renderBooks(books) {
   })
 }
 
+function lateCharacter(books) {
+  const main = document.querySelector('main')
+
+  books.forEach(book => {
+    book.characters.forEach(character => {
+      
+    })
+  })
+}
+
+function fifthInTheSeries(books) {
+  const main = document.querySelector('main')
+  let book = books[5]
+    const h1 = document.createElement('h1')
+    h1.innerHTML = book.name
+    main.appendChild(h1)
+}
+
+
+function totalPageCount(books) {
+  let pageTotal = 0
+  books.forEach(book => {
+    pageTotal += parseInt(book.numberOfPages)
+  })
+  const main = document.querySelector('main')
+  const h2 = document.createElement('h2')
+  h2.innerHTML = pageTotal
+  main.appendChild(h2)
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
